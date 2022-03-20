01df262ce4377799281ddbbedfb88534b318d880
MaameSekyere <maameant@gmail.com>
2022-03-20 18:02:26 -0400

    added password selections

--- Develop/script.js
+++ Develop/script.js
@@ -9,10 +9,67 @@ var passwordCharacters = {
   },
 };
 
-
-
-
-
+// Password Choices Selection
+var passwordSelection = {
+  lowercase: [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ],
+  uppercase: [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ],
+  specialCharacters: ["!", "@", "#", "$", "%", "&"],
+  numeric: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
