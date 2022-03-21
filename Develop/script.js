3524607c4cf3eaa3de119e7d9cc66073f7c25746
MaameSekyere <maameant@gmail.com>
2022-03-21 02:31:24 -0400

    revised password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,207 +1,117 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 
 // Password varaible and objects
-var passwordCharacters = {
-  numberLength: 8,
-  value: "",
-  characters: []
-  reset: function () {
-    (this.value = ""), (numberLength = 8); (this.characters);
-  },
-};
+var numberLength = 8;
+var characters = [];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var specialSymbol = ["!", "@", "#", "$", "%", "*", "&"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Password Choices Selection
-var passwordSelection = {
-  lowercase: [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ],
-  uppercase: [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ],
-  specialSymbol: ["!", "@", "#", "$", "%", "*", "&"],
-  numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-};
+//Functions to alert user
+function alertUser() {
+  characters = [];
 
-// Length of password function
-var passwordLengthFunction = function () {
-  var passwordLength = window.prompt(
-    "Please specify number of characters from 8 to 20."
+  numberLength = parseInt(
+    alert("Select a number between 8 to 128 for password length")
   );
-  if (passwordLength === "" || passwordLength === null) {
-    window.alert("Please try again, search bar is empty.");
+  if (isNaN(numberLength) || numberLength < 8 || numberLength > 128) {
+    alert("Select a number between 8 to 128 for password length");
+    return false;
   }
-  if (passwordLength >= 8 && passwordLength <= 20) {
-    passwordCharacters.numberLength = passwordLength;
-  } else {
-    window.alert("Please enter a number between 8 and 20!");
+
+  // Prompt user to select if they would like lowercase, uppercase, numbers, special characters in password
+  if (confirm("Do you want a lowercase letter in your password?")) {
+    characters = characters.concat(lowerCase);
   }
-};
-passwordLengthFunction();
 
-// Generate random number
-var generateRandomPassword = function () {
-  for (var n = 0; n < passwordCharacters.numberLength; n++) {
-    var randomNumber = Math.floor(Math.random() * passwordCharacters.characters.length);
-    passwordCharacters.value += passwordCharacters.characters[randomNumber];
+  if (confirm("Do you want a uppercase letter in your password?")) {
+    characters = characters.concat(upperCase);
   }
-};
 
-generateRandomPassword();
-
-// Lowercase letter option
-var lowerCase = function () {
-  var lowercaseLetter = window.prompt(
-    "Are there any lowercase letters in your password? Choose 'yes' or 'no'."
-  );
-  if (lowercaseLetter === "" || lowercaseLetter === null) {
-    window.alert("Empty search bar. Try again");
-    lowercaseLetter = window.prompt(
-      "Would you like to use lowercase? Choose 'yes' or 'no'."
-    );
-  } else {
-    lowercaseLetter = lowercaseLetter.toLowerCase();
-
-    switch (lowercaseLetter) {
-        case 'yes':
-          passwordCharacters = passwordCharacters.concat(passwordSelection.lowercase);
-          break;
-        case 'no':
-          break;
-        default: 
-          window.alert("Try again.");
+  if (confirm("Do you want a special character in your password?")) {
+    characters = characters.concat(specialSymbol);
   }
-};
-
-lowerCase();
-
-// Uppercase letter option
-var upperCase = function () {
-  var uppercaseLetter = window.prompt(
-    "Are there any uppercase letters in your password? Choose 'yes' or 'no'."
-  );
-  if (uppercaseLetter === "" || uppercaseLetter === null) {
-    window.alert("Empty search bar. Try again");
-    upperCase();
-  } else {
-    uppercaseLetter = uppercaseLetter.toLowerCase();
-
-    switch (uppercaseLetter) {
-        case 'yes':
-          passwordInfo. = passwordInfo.characters.concat(passwordChoices.uppercase);
-          break;
-        case 'no':
-          break;
-        default: 
-          window.alert("Please make a valid selection. Try again.");
-          uppercaseFunction();
-      }
+  if (confirm("Do you want numbers in your password?")) {
+    characters = characters.concat(numbers);
   }
-};
-
-upperCase();
-
-// Numeric option
-var numberFunction = function () {
-  var numberSelection = window.prompt(
-    "Would you like to use numbers? Choose 'yes' or 'no'."
-  );
-  if (numberSelection === "" || numberSelection === null) {
-    window.alert("Fields left empty. Please try again");
-    numberFunction();
-  } else {
-    numberSelection = numberSelection.toLowerCase();
-  }
-};
-
-numberFunction();
-
-// Special Character option
-var specialFunction = function () {
-  var specialCharacter = window.prompt(
-    "Would you like to use special characters? Choose 'yes' or 'no'."
-  );
-  if (specialCharacter === "" || specialCharacter === null) {
-    window.alert("Fields left empty. Please try again");
-    specialFunction();
-  } else {
-    specialCharacter = specialCharacter.toLowerCase();
-  }
-};
-
-specialFunction();
-
-function generatePassword() {
-  passwordCharacters.reset();
-  passwordLengthFunction();
-  lowerCase();
-  upperCase();
-  numberFunction();
-  specialFunction();
-  generateRandomPassword();
+  return true;
 }
 
-generatePassword();
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < numberLength; i++) {
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    password = password + characters[randomNumber];
+  }
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var alertUser = alertUser();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  if (alertUser) {
+    var getPassword = generatePassword();
+    passwordText.value = getPassword;
+  } else {
+    passwordText.value = "";
+  }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
